0ea51dbc0de44a088c4b13d65652d85b5aedf5e5
Nicholas S. Kern <nkern@berkeley.edu>
2017-03-25 19:54:20 -0700

    modified:   emulator.py

--- emupy/emulator.py
+++ emupy/emulator.py
@@ -441,8 +441,8 @@ class Emu(object):
             self.weights_cv = weights
             self.weights_err_cv = weights_err
 
-        self.weights_cv *= self.w_norm
-        self.weights_err_cv *= self.w_norm
+        self.weights_cv /= self.w_norm
+        self.weights_err_cv /= self.w_norm
 
         if output == True:
             return self.recon_cv, self.recon_err_cv, self.recon_err_cov, self.weights_cv, self.weights_err_cv
